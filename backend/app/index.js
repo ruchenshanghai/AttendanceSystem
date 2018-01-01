7f7d851fc2950c46cbb3f48083d7a1645c1e6b39
濡尘赏海 <996956441@qq.com>
2018-01-01 13:38:33 +0800

    need to do: all ok， add api list, deploy

--- backend/app/index.js
+++ backend/app/index.js
@@ -32,15 +32,6 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + '/public', 'index.html'));
 });
 
-// let moment = require('moment');
-// // console.log(moment('2017-12-1').format('YYYY-MM-DD HH:mm:ss'));
-// let temp = new Date('ss');
-// console.log(temp.toString());
-// console.log(Date.parse('ss 12'));
-// const md5 = require('md5');
-// let password = md5('database2017');
-// console.log(password);
-
 
 server.listen(config.server_port, function () {
   console.log('Express server listening on port %d in %s mode', config.server_port, app.get('env'));
